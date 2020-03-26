76d57aa982fc35f7472e1128cd7cf416fe148444
pedroguarderas <pedro.felipe.guarderas@gmail.com>
2020-03-25 21:08:58 -0500

    Adding load and estimation scripts

--- R/run_project.R
+++ R/run_project.R
@@ -2,6 +2,8 @@ message( paste0( rep( '-', 100 ), collapse = '' ) )
 
 source( 'R/load_packages.R', encoding = 'UTF-8', echo = FALSE )
 source( 'R/parameters.R', encoding = 'UTF-8', echo = FALSE )
+source( 'R/load_covid_19_info.R', encoding = 'UTF-8', echo = FALSE )
+source( 'R/estimation_covid_19.R', encoding = 'UTF-8', echo = FALSE )
 source( 'R/SIR_model_example.R', encoding = 'UTF-8', echo = FALSE )
 source( 'R/SIR_model_ecu_covid_19.R', encoding = 'UTF-8', echo = FALSE )
 
