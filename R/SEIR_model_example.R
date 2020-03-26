0db8b3a8a26317e2aeb484d14abe8f11ff5a1b16
pedroguarderas <pedro.felipe.guarderas@gmail.com>
2020-03-25 23:27:28 -0500

    Example of the SEIR model

---
+++ R/SEIR_model_example.R
@@ -0,0 +1,28 @@
+source( 'R/solvers.R', encoding = 'UTF-8', echo = FALSE )
+
+# Modelo SEIR ---------------------------------------------------------------------------------------
+S0 <- 0.9
+E0 <- 0.09
+I0 <- 0.01
+R0 <- 0.0
+
+alpha <- 0.9
+beta <- 0.2
+sigma <- 0.5
+mu <- 0.0001
+nu <- 0.001
+
+n <- 1e3
+t <- seq( 0, 60, length.out = n )
+
+sol <- euler_solver_seir( t, alpha, beta, sigma, mu, nu, S0, E0, I0, R0 )
+
+plt_solv <- ggplot() +
+  geom_line( aes( x = t, y = sol$S ), color = 'purple' ) + 
+  geom_line( aes( x = t, y = sol$E ), color = 'dodgerblue3' ) + 
+  geom_line( aes( x = t, y = sol$I ), color = 'orange' ) +
+  geom_line( aes( x = t, y = sol$R ), color = 'darkgreen' ) +
+  scale_y_continuous( limits = c( 0, 1 ) ) +
+  theme_bw()
+
+plot( plt_solv )
