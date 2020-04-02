51dfdc579bba85b163056d3ec832e01a379bddc9
pedroguarderas <pedro.felipe.guarderas@gmail.com>
2020-04-02 17:18:30 -0500

    Separation in sections

--- R/run_project.R
+++ R/run_project.R
@@ -1,10 +1,14 @@
 message( paste0( rep( '-', 100 ), collapse = '' ) )
 
+# Configuration
 source( 'R/load_packages.R', encoding = 'UTF-8', echo = FALSE )
 source( 'R/parameters.R', encoding = 'UTF-8', echo = FALSE )
-source( 'R/load_covid_19_info.R', encoding = 'UTF-8', echo = FALSE )
+
+# Statistics
 source( 'R/estimation_covid_19.R', encoding = 'UTF-8', echo = FALSE )
 source( 'R/graphs_statistics_covid_19.R', encoding = 'UTF-8', echo = FALSE )
+
+# Modelling
 source( 'R/SIR_model_example.R', encoding = 'UTF-8', echo = FALSE )
 source( 'R/SIR_model_ecu_covid_19.R', encoding = 'UTF-8', echo = FALSE )
 
