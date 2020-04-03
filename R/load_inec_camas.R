f9d50ce4d3bc3feb927e8baacf1fc884018aa1ab
pedroguarderas <pedro.felipe.guarderas@gmail.com>
2020-04-02 23:24:33 -0500

    Loading information of 2018 related to beds

--- R/load_inec_camas.R
+++ R/load_inec_camas.R
@@ -16,6 +16,13 @@ camas_2017 <- read.csv( file = paste0( par$data, 'MSP_camas_hospitales/camas_201
 camas_2017 <- as.data.table( camas_2017 )
 camas_2017[ , y := 2017 ]
 
+# Camas año 2018 -----------------------------------------------------------------------------------
+camas_2018 <- read.csv( file = paste0( par$data, 'MSP_camas_hospitales/camas_2018.csv' ), 
+                        header = TRUE, sep = ';', fileEncoding = 'Latin1' )
+
+camas_2018 <- as.data.table( camas_2018 )
+camas_2018[ , y := 2018 ]
+
 # Recursos salud año 2017 --------------------------------------------------------------------------
 ras_2017 <- read.csv( file = paste0( par$data, 'MSP_recursos_salud/RAS_2017.csv' ), 
                         header = TRUE, sep = ';', fileEncoding = 'Latin1' )
@@ -24,7 +31,9 @@ ras_2017 <- as.data.table( ras_2017 )
 ras_2017[ , y := 2017 ]
 
 # Guardando resultados -----------------------------------------------------------------------------
-save( camas_2016, camas_2017, file = paste0( par$RData, 'inec_camas_hosp_ecu.RData' ) )
+save( camas_2016, camas_2017, camas_2018,
+      file = paste0( par$RData, 'inec_camas_hosp_ecu.RData' ) )
+
 save( ras_2017, file = paste0( par$RData, 'inec_recurso_salud_ecu.RData' ) )
 
 message( paste0( rep( '-', 100 ), collapse = '' ) )
