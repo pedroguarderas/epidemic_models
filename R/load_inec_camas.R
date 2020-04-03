f2f0e81ae5f17e67f2b5ffbe1c6f061f6f554b48
pedroguarderas <pedro.felipe.guarderas@gmail.com>
2020-04-02 23:27:08 -0500

    Renaming RData

--- R/load_inec_camas.R
+++ R/load_inec_camas.R
@@ -32,9 +32,9 @@ ras_2017[ , y := 2017 ]
 
 # Guardando resultados -----------------------------------------------------------------------------
 save( camas_2016, camas_2017, camas_2018,
-      file = paste0( par$RData, 'inec_camas_hosp_ecu.RData' ) )
+      file = paste0( par$RData, 'inec_beds_hospital_ecu.RData' ) )
 
-save( ras_2017, file = paste0( par$RData, 'inec_recurso_salud_ecu.RData' ) )
+save( ras_2017, file = paste0( par$RData, 'inec_resource_health_ecu.RData' ) )
 
 message( paste0( rep( '-', 100 ), collapse = '' ) )
 rm( list = ls()[ !( ls() %in% c( 'par' ) ) ] )
