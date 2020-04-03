4691428dc222bab4571641036d3de46ad9c9027f
pedroguarderas <pedro.felipe.guarderas@gmail.com>
2020-04-03 16:00:19 -0500

    Adding more statistics

--- R/statistics_covid_19.R
+++ R/statistics_covid_19.R
@@ -19,52 +19,58 @@ covid[ , t := interval( tmin, t ) / ddays( 1 ) + 1 ]
 covid_ts <- covid[ , list( i = sum( i, na.rm = TRUE ), 
                            d = sum( d, na.rm = TRUE ), 
                            r = sum( r, na.rm = TRUE ) ), 
-                   by = list( t ) ]
-setorder( covid_ts, t )
+                   by = list( date ) ]
+setorder( covid_ts, date )
 
 # covid_ts[ , t := as.POSIXct( date ) ]
 # covid_ts[ , t := interval( min( t ), t ) / ddays( 1 ) + 1 ]
 
-covid_ts[ , ri := shift( i, fill = 0 ) ]
-covid_ts[ , ri := i / ri - 1 ]
-covid_ts[ !is.finite( ri ), ri := 0 ]
-covid_ts[ , rd := shift( d, fill = 0 ) ]
-covid_ts[ , rd := d / rd - 1 ]
-covid_ts[ !is.finite( rd ), rd := 0 ]
-covid_ts[ , rr := shift( r, fill = 0 ) ]
-covid_ts[ , rr := r / rr - 1 ]
-covid_ts[ !is.finite( rr ), rr := 0 ]
-covid_ts[ , beta := 0 ]
-covid_ts[ i + d + r > 0, beta := r / ( i + d + r ) ]
-covid_ts[ , eta := 0 ]
-covid_ts[ i + d + r > 0, eta := d / ( i + d + r ) ]
+covid_dy <- covid[ , list( i = sum( i, na.rm = TRUE ), 
+                           d = sum( d, na.rm = TRUE ), 
+                           r = sum( r, na.rm = TRUE ) ), 
+                   by = list( t ) ]
+setorder( covid_dy, t )
+
+covid_dy[ , ri := shift( i, fill = 0 ) ]
+covid_dy[ , ri := i / ri - 1 ]
+covid_dy[ !is.finite( ri ), ri := 0 ]
+covid_dy[ , rd := shift( d, fill = 0 ) ]
+covid_dy[ , rd := d / rd - 1 ]
+covid_dy[ !is.finite( rd ), rd := 0 ]
+covid_dy[ , rr := shift( r, fill = 0 ) ]
+covid_dy[ , rr := r / rr - 1 ]
+covid_dy[ !is.finite( rr ), rr := 0 ]
+covid_dy[ , beta := 0 ]
+covid_dy[ i + d + r > 0, beta := r / ( i + d + r ) ]
+covid_dy[ , eta := 0 ]
+covid_dy[ i + d + r > 0, eta := d / ( i + d + r ) ]
 
 
 # Time series by country or region
-covid_reg_ts <- covid[ , list( i = sum( i, na.rm = TRUE ), 
+covid_reg_dy <- covid[ , list( i = sum( i, na.rm = TRUE ), 
                                d = sum( d, na.rm = TRUE ), 
                                r = sum( r, na.rm = TRUE ) ), 
                        by = list( country_region, t ) ]
-# covid_reg_ts <- covid_reg_ts[ i > 0 ]
-setorder( covid_reg_ts, country_region, t )
+# covid_reg_dy <- covid_reg_dy[ i > 0 ]
+setorder( covid_reg_dy, country_region, t )
 
-# covid_reg_ts[ , t := as.POSIXct( date ) ]
-# covid_reg_ts[ , tmin := min( t ), by = list( country_region ) ]
-# covid_reg_ts[ , t := interval( tmin, t ) / ddays( 1 ) + 1 ]
+# covid_reg_dy[ , t := as.POSIXct( date ) ]
+# covid_reg_dy[ , tmin := min( t ), by = list( country_region ) ]
+# covid_reg_dy[ , t := interval( tmin, t ) / ddays( 1 ) + 1 ]
 
-covid_reg_ts[ , ri := shift( i, fill = 0 ), by = country_region ]
-covid_reg_ts[ , ri := i / ri - 1 ]
-covid_reg_ts[ !is.finite( ri ), ri := 0 ]
-covid_reg_ts[ , rd := shift( d, fill = 0 ), by = country_region ]
-covid_reg_ts[ , rd := d / rd - 1 ]
-covid_reg_ts[ !is.finite( rd ), rd := 0 ]
-covid_reg_ts[ , rr := shift( r, fill = 0 ), by = country_region ]
-covid_reg_ts[ , rr := r / rr - 1 ]
-covid_reg_ts[ !is.finite( rr ), rr := 0 ]
-covid_reg_ts[ , beta := 0 ]
-covid_reg_ts[ i + d + r > 0, beta := r / ( i + d + r ) ]
-covid_reg_ts[ , eta := 0 ]
-covid_reg_ts[ i + d + r > 0, eta := d / ( i + d + r ) ]
+covid_reg_dy[ , ri := shift( i, fill = 0 ), by = country_region ]
+covid_reg_dy[ , ri := i / ri - 1 ]
+covid_reg_dy[ !is.finite( ri ), ri := 0 ]
+covid_reg_dy[ , rd := shift( d, fill = 0 ), by = country_region ]
+covid_reg_dy[ , rd := d / rd - 1 ]
+covid_reg_dy[ !is.finite( rd ), rd := 0 ]
+covid_reg_dy[ , rr := shift( r, fill = 0 ), by = country_region ]
+covid_reg_dy[ , rr := r / rr - 1 ]
+covid_reg_dy[ !is.finite( rr ), rr := 0 ]
+covid_reg_dy[ , beta := 0 ]
+covid_reg_dy[ i + d + r > 0, beta := r / ( i + d + r ) ]
+covid_reg_dy[ , eta := 0 ]
+covid_reg_dy[ i + d + r > 0, eta := d / ( i + d + r ) ]
 
 # By geographic position
 covid_geo <- covid[ , list( i = sum( i, na.rm = TRUE ), 
@@ -72,7 +78,7 @@ covid_geo <- covid[ , list( i = sum( i, na.rm = TRUE ),
                             r = sum( r, na.rm = TRUE ) ), 
                     by = list( lat, long ) ]
 
-save( covid, covid_geo, covid_ts, covid_reg_ts,
+save( covid, covid_geo, covid_ts, covid_reg_dy, covid_dy,
       file = paste0( par$RData, 'covid_19_statistics.RData' ) )
 
 
